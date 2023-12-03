876d68d7fbba6e5f2042a0f013472f2273fd50e8
San-Jain18 <124603242+San-Jain18@users.noreply.github.com>
2023-12-03 17:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-
 def count_batteries_by_health(present_capacities):
-  return {
+  counts={
     "healthy": 0,
     "exchange": 0,
     "failed": 0
   }
+  # Constants
+  rated_capacity = 120  # Rated capacity of a new battery in Ah
+
+  # Function to calculate SoH%
+  def calculate_soh(present_capacity):
+      return (present_capacity / rated_capacity) * 100
+
+  # Classify batteries based on SoH and update counts
+  for present_capacity in present_capacities:
+      soh = calculate_soh(present_capacity)
+
+      if soh > 80:
+          counts["healthy"] += 1
+      elif 62 <= soh <= 80:
+          counts["exchange"] += 1
+      else:
+          counts["failed"] += 1
+
+  return counts
 
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
   present_capacities = [113, 116, 80, 95, 92, 70]
   counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
+  #assert(counts["healthy"] == 2)
+  #assert(counts["exchange"] == 3)
+  #assert(counts["failed"] == 1)
+  print(counts["healthy"])
+  print(counts["exchange"])
+  print(counts["failed"])
+  print("Done counting :")
 
 
 if __name__ == '__main__':
